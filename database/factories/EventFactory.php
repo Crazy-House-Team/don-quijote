4ee3e624cb17aa750851f17894dd0c048b5576f6
Trabanco <btrabanco@gmail.com>
2022-08-23 14:01:09 +0200

    create event factory

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence($nbWords = 6, $variableNbWords = true),
+            'resume' => $this->faker->realText($maxNbChars = 50, $indexSize = 1),
+            'description' => $this->faker->realText($maxNbChars = 200, $indexSize = 2),
+            'place' => $this->faker->company(),
+            'address' => $this->faker->streetAddress(),
+            'date' => $this->faker->date($format = 'd-m-Y'),
+            'time' => $this->faker->time($format = 'H:i:s'),
+            'img' => $this->faker->imageUrl($width = 640, $height = 480),
+            'max_participants' => $this->faker->numberBetween($min = 10, $max = 100),
+        ];
+    }
+}
