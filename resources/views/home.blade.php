7e1b2f580c793d6682be93771975646530a9dc86
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-08-31 17:58:17 +0200

    fix: div tag

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,26 +4,24 @@
 
 
 <h1>home</h1>
-    @include('components.carousel');
+@include('components.carousel');
 
-    <h2 class="text-3xl font-bold mb-12 text-center">Eventos activos</h2>
+<h2 class="text-3xl font-bold mb-12 text-center">Eventos activos</h2>
 <div class="cards-container">
 
 
     @foreach ($events as $event)
-        @if(!$event->favorite && $event->date > date('Y-m-d'))
-            @include('components.card')
-        @endif
+    @if(!$event->favorite && $event->date > date('Y-m-d'))
+    @include('components.card')
+    @endif
     @endforeach
 
 
     @foreach ($events as $event)
-        @if($event->date <= date('Y-m-d'))
-            @include('components.card')
-        @endif
+    @if($event->date <= date('Y-m-d'))
+    @include('components.card')
+    @endif
     @endforeach
-
-    @include('components.footer');
-
-
+</div>
+@include('components.footer');
 @endsection
