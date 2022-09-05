7e87486ac08f7bf9cb7cebd9c597336958ab2f14
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-09-05 17:42:55 +0200

    test upload

--- tests/Feature/EventControllerUpdateTest.php
+++ tests/Feature/EventControllerUpdateTest.php
@@ -24,11 +24,33 @@ class EventControllerUpdateTest extends TestCase
             'is_admin' => true,
         ]);
 
-        $event = Event::factory()->create([
-            "title" => "title-test",
-        ]);
+        $event = Event::factory()->create();
 
         $this->assertCount(1, Event::all());
 
+        $response = $this->actingAs($admin)->patch(\route('updateEvent', $event->id), ['title' => 'title-upload']);
+
+        $response->assertStatus(200);
+
+        $this->assertEquals('title-upload', Event::first()->title);
+    }
+
+    public function test_event_cannot_be_updated_by_non_admin_user()
+    {
+        $this->withExceptionHandling();
+
+        $user = User::factory()->create([
+            'is_admin' => false,
+        ]);
+
+        $event = Event::factory()->create();
+
+        $this->assertCount(1, Event::all());
+
+        $response = $this->actingAs($user)->patch(\route('updateEvent', $event->id), ['title' => 'title-upload']);
+
+        $updatedEvent = Event::find($event->id);
+
+        $this->assertEquals('title-test', $updatedEvent->title);
     }
 }
